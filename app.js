d0c3f24636cfc98ccea89b16a90527498e50d1a0
Brian Levin <bml201095@gmail.com>
2020-08-12 16:57:28 -0400

    made xmlhttprequest to fetch the api

--- app.js
+++ app.js
@@ -2,5 +2,12 @@ document.querySelector('.get-jokes').addEventListener('click', getJokes);
 
 function getJokes(e) {
   const number = document.querySelector('input[type="number"]').value;
+  
+  const xhr = new XMLHttpRequest();
+
+
+  xhr.open('GET', `http://api.icndb.com/jokes/random/${number}`, true);
+
+  xhr.send();
 
 }
